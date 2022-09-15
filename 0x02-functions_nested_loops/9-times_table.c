f4f0104ca5419a847d122c07684faa8acebbbff6
Crystalxclusive <abdulsamadbayoade@gmail.com>
2022-09-14 19:34:21 -0700

    9 times table)

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,28 +11,27 @@ void times_table(void)
 
 		for (i = 0; i <= 9; i++)
 		{
-			for (j = 0; j <= 9; j++)
+			_putchar('0');
+
+			for (j = 1; j <= 9; j++)
 			{
+				_putchar(',');
+				_putchar(' ');
+
+
 				n = i * j;
-					if ((n / 10) == 0)
-					{
-					if (j != 0)
-						_putchar(' ');
-						_putchar(n + '0');
-					if (j == 9)
-						continue;
-						_putchar(',');
-						_putchar(' ');
-					}
-					else
-					{
-						_putchar((n / 10) + '0');
-						_putchar((n % 10) + '0');
-					if (j == 9)
-						continue;
-						_putchar(',');
-						_putchar(' ');
-					}
-					_putchar('\n');
+				
+				if (n <= 9)
+				{
+					_putchar(' ');
+				}
+
+				else
+				{
+					_putchar((n / 10) + '0');
+				}
+
+				_putchar((n % 10) + '0');
+			}
+			_putchar('\n');
 		}
-}
