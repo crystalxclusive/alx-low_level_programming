e5fcb8648653799862f7819672744e4cfcdb5a6d
Crystalxclusive <abdulsamadbayoade@gmail.com>
2022-09-16 08:06:02 -0700

    More

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,16 +7,16 @@
  */
 void more_numbers(void)
 {
-	int i, j = 0;
+	int j = 0;
 
 	for (; j < 10; j++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (int i = 0; i <= 14; i++)
 		{
 			if (i > 9)
 			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
 			}
 		}
 		_putchar('\n');
