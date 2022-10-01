aa8a2b7f6532fd77abc71e856d7415b7bfea231c
Crystalxclusive <abdulsamadbayoade@gmail.com>
2022-10-01 00:48:12 -0700

    Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,7 +9,7 @@
 int isInteger(const char *s)
 {
 	int i = 0;
-	
+
 	while (s[i] != '\0')
 	{
 		if (s[i] < '0' || s[i] > '9')
@@ -30,7 +30,7 @@ int main(int argc, char const *argv[])
 {
 	int i = 0, coinUsed = 0, coin = 0;
 	int coins[] = {25, 10, 5, 2, 1};
-	
+
 	if (argc != 2)
 	{
 		printf("Error\n");
